185a912f7883482015b4209e272bd580b5c87533
Rouf1224 <ahmadroufr24@gmail.com>
2025-04-15 15:44:35 +0700

    struct 2

--- struct.cpp
+++ struct.cpp
@@ -6,3 +6,18 @@ struct mahasiswa{
 string nama;
 string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140097";
+    mhs.nama = "ahmad";
+    mhs.alamat = "klaten";
+
+    cout << "Mahasiswa NIM =";
+    cin >> mhs.nim;
+    cout << "Mahasiswa Nama =";
+    cin >> mhs.nama;
+    cout << "Mahasiswa Alamat =";
+    cin >> mhs.alamat;
+}
